6c5d3848e657af695c4fdb17a3e7a795119d6f92
Phuc Nguyen <stylephuc@gmail.com>
2015-07-20 21:35:23 -0700

    update

---
+++ ColorMakerWithSlider/ColorMakerWithSlider/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  ColorMakerWithSlider
+//
+//  Created by Phuc Nguyen on 7/20/15.
+//  Copyright (c) 2015 Phuc Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redSlider: UISlider!
+   
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    
+    override func viewDidLoad() {
+        
+    }
+    @IBAction func changeColor(sender: UISlider) {
+        let r = CGFloat(redSlider.value)
+        let g = CGFloat(greenSlider.value)
+        let b = CGFloat(blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+        
+    }
+}
+
